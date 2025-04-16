8441c241e20e446703466bda059b2c3d943c537f
Hemantpayasi <106426032+Hemantpayasi@users.noreply.github.com>
2025-04-16 21:08:06 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "Hemant@google.com"
+var accountPassword = "12345"
+accountCity = "Pali"
+let accountState;
+
+// accountId = 2 // Not allowed
+
+accountEmail = "payasi@google.com"
+accountPassword = "11111"
+accountCity = "Bhopal"
+
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
