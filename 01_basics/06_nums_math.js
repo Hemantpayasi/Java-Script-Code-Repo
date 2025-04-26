7a5e56e599037ba51c86a02982fef8636aa9bb44
Hemantpayasi <106426032+Hemantpayasi@users.noreply.github.com>
2025-04-26 06:42:23 +0000

    Math and num

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,33 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNumber = 34.84856
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// +++++++++++++++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++++
+let num = Number.MAX_VALUE
+// console.log(num)
+
+// console.log(Math.abs(-53))
+// console.log(Math.round(5.3))
+// console.log(Math.ceil(5.3))
+// console.log(Math.floor(5.3))
+
+// console.log(Math.max(5, 7, 1, 9, 5))
+// console.log(Math.min(5, 7, 1, 9, 5))
+
+console.log(Math.random().toFixed(2)*100)
+
+const min = 15
+const max = 20
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min))
